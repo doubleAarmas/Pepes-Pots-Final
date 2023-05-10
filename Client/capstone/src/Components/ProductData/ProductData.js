a84b68ced4bee5caaf3737af1867f16b463324fe
Alfredo <aarma-028@gmail.com>
2023-05-10 16:08:38 -0400

    right before adding payments

--- Client/capstone/src/Components/ProductData/ProductData.js
+++ Client/capstone/src/Components/ProductData/ProductData.js
@@ -2,25 +2,25 @@ const products = [
   {
     id: 1,
     title: "Kale Bowls",
-    image: require("../../Images/Kale-bowl.jpeg"),
+    image: "../../Images/Kale-bowl.jpeg",
     price: 15,
   },
   {
     id: 2,
     title: "Bok-Choy Bowls",
-    image: require("../../Images/bok-choy-bowl.jpg"),
+    image: "../../Images/bok-choy-bowl.jpg",
     price: 15,
   },
   {
     id: 3,
     title: "Lettuce Bowls",
-    image: require("../../Images/Lettuce-bowl.jpg"),
+    image: "../../Images/Lettuce-bowl.jpg",
     price: 15,
   },
   {
     id: 4,
     title: "Mint Bowls",
-    image: require("../../Images/Mint-bowl.jpg"),
+    image: "../../Images/Mint-bowl.jpg",
     price: 15,
   },
   {
@@ -46,7 +46,7 @@ const products = [
 function getProductData(id) {
   let productData = products.find((product) => product.id === id);
 
-  if (productData == undefined) {
+  if (productData === undefined) {
     console.log("Product data does not exist for ID: " + id);
     // adding the return statement here as well to make it easier to read
     return undefined;
