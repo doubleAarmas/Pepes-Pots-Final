a2bd14b0c5b3653797503a8d5e1fdf5c7fb84539
Alfredo <aarma-028@gmail.com>
2023-05-15 16:10:50 -0400

    adjusting descriptions, error has been limited to just Stripe

--- Server/Server.js
+++ Server/Server.js
@@ -1,17 +1,13 @@
-// Microgreens: price_1N6KEEJU04y3N9ID3fCihd2I
-// Kale Bowl: price_1N6KFDJU04y3N9IDWxqQ4fF6
-// Lettuce Bowl: price_1N6KIsJU04y3N9IDAna73HBp
 const express = require("express");
 const app = express();
+app.use(express.static("public"));
 var cors = require("cors");
 const fs = require("fs");
-const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 
 app.use(cors());
-//double check if it should be public or src
-app.use(express.static("public"));
 app.use(express.json());
 
 const productsList = fs.readFileSync("./Data/ProductData.json");
@@ -40,30 +36,38 @@ stripe wants
 ]
 make a new variable to hold what stripe wants
     */
+  console.log(req.body);
   const items = req.body.items;
-  let lineItems = [];
-  items.forEach((item) => {
-    lineItems.push({
-      price: item.id,
-      quantity: item.quantity,
+  if (items && Array.isArray(items)) {
+    let lineItems = [];
+    items.forEach((item) => {
+      lineItems.push({
+        price: item.id,
+        quantity: 1,
+
+        // quantity: item.quantity,
+      });
     });
-  });
 
-  //making a payment with the lineItems that Stripe wants
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: lineItems,
-    mode: "payment",
-    success_url: "http://localhost:3000/success",
-    cancel_url: "http://localhost:3000/cancel",
-  });
+    //making a payment with the lineItems that Stripe wants
+    const session = await stripe.checkout.sessions.create({
+      // payment_method_types: ["card"],
+      line_items: lineItems,
+      mode: "payment",
+      success_url: "http://localhost:3000/success",
+      cancel_url: "http://localhost:3000/cancel",
+      automatic_tax: { enabled: true },
+    });
 
-  //sending the created session URL to the front end so the user can checkout with stripe
-  res.send(
-    JSON.stringify({
-      url: session.url,
-    })
-  );
+    //sending the created session URL to the front end so the user can checkout with stripe
+    res.send(
+      JSON.stringify({
+        url: session.url,
+      })
+    );
+  } else {
+    res.status(400).send("Invalid request");
+  }
 });
 
 app.listen(8080, () => console.log("Listening on port 8080"));
