afb98f9d73caf0fa3df83d58118443d98a67cd7e
Alfredo <aarma-028@gmail.com>
2023-05-16 15:30:21 -0400

    cors is working although there are errors in the console

--- Server/Server.js
+++ Server/Server.js
@@ -1,89 +1,13 @@
-// const express = require("express");
-// const app = express();
-// app.use(express.static("public"));
-// var cors = require("cors");
-// const fs = require("fs");
-
-// require("dotenv").config();
-// const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
-
-// app.use(cors());
-// app.use(express.json());
-
-// const productsList = fs.readFileSync("./Data/ProductData.json");
-// const parsedData = JSON.parse(productsList);
-
-// app.get("/products", (req, res) => {
-//   const products = parsedData;
-//   res.send(products);
-// });
-
-// app.post("/checkout", async (req, res) => {
-//   /*req.body.items
-// [
-//     {
-//         id:1,
-//         quantity:3
-//     }
-// ]
-
-// stripe wants
-// [
-//     {
-//         price:1,
-//         quantity: 3
-//     }
-// ]
-// make a new variable to hold what stripe wants
-//     */
-//   console.log(req.body);
-//   const items = req.body.items;
-//   if (items && Array.isArray(items)) {
-//     let lineItems = [];
-//     items.forEach((item) => {
-//       lineItems.push({
-//         price: item.id,
-//         quantity: 1,
-
-//         // quantity: item.quantity,
-//       });
-//     });
-
-//     //making a payment with the lineItems that Stripe wants
-//     const session = await stripe.checkout.sessions.create({
-//       // payment_method_types: ["card"],
-//       line_items: lineItems,
-//       mode: "payment",
-//       success_url: "http://localhost:3000/success",
-//       cancel_url: "http://localhost:3000/cancel",
-//       automatic_tax: { enabled: true },
-//     });
-
-//     //sending the created session URL to the front end so the user can checkout with stripe
-//     res.send(
-//       JSON.stringify({
-//         url: session.url,
-//       })
-//     );
-//   } else {
-//     res.status(400).send("Invalid request");
-//   }
-// });
-
-// app.listen(8080, () => console.log("Listening on port 8080"));
-
 const express = require("express");
 const app = express();
-app.use(express.static("public"));
 var cors = require("cors");
 const fs = require("fs");
 
 require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
-
+app.use(express.static("public"));
 app.use(cors());
 app.options("*", cors()); // Enable pre-flight request for all routes
-
 app.use(express.json());
 
 const productsList = fs.readFileSync("./Data/ProductData.json");
@@ -110,7 +34,7 @@ app.post("/checkout", async (req, res) => {
       cancel_url: "http://localhost:3000/cancel",
     });
 
-    res.redirect(303, session.url);
+    res.json({ url: session.url }); // Send the session URL as a JSON response
   } else {
     res.status(400).send("Invalid request");
   }
