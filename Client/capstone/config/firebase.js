3384ec8c844ebad007a3786ba7fc66011a1fcca3
Alfredo <aarma-028@gmail.com>
2023-06-12 13:19:10 -0400

    prepping firebase

---
+++ Client/capstone/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBq6nqsFQOmjweQPCA7FkZjnHzqqzq5S6M",
+  authDomain: "pepe-s-pots.firebaseapp.com",
+  projectId: "pepe-s-pots",
+  storageBucket: "pepe-s-pots.appspot.com",
+  messagingSenderId: "202754616166",
+  appId: "1:202754616166:web:c60df2bcca7231a602b6a7",
+  measurementId: "G-G6QNWH56WP",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
